4d6c02924ab9331ba7b57d9232f3a2d9fc3fe5d2
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-05 12:28:38 +0530

    add login boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Login from './components/account/Login';
+
 
 function App() {
   return (
     <div className="App">
-      Hello Amit
+      <Login/>
     </div>
   );
 }
