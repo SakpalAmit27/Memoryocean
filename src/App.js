44b1ceaa7341a22e4646586bd972fd1bbff76afa
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-05 13:47:48 +0530

    installed and setupped chakra ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import Login from './components/account/Login';
-
+import {ChakraProvider} from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
+    <ChakraProvider>
       <Login/>
-    </div>
+
+    </ChakraProvider>
+
+
   );
 }
 
