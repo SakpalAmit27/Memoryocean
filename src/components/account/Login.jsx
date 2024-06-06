f9d4883544d614c4c79d78e0b4e447a7fd38c8a8
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-06 18:36:55 +0530

    removed css from login

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { Box, Button, Image, Input, Stack, VStack, Center, Text } from '@chakra-ui/react';
 import website_icon from '../../assets/icon.png';
-import './Login.css';
-import login_background from '../../assets/login-background.png'
+import login_background from '../../assets/login-background.png';
 
 const Login = () => {
   return (
-    <>
-
-
-    <Center className='whole'>
- 
-      <Box className='login-form'>
-        <Stack spacing={5}>  {/* Reduce the spacing between elements */}
-          <Box className='logo-id'>
-            <Image src={website_icon} className='logo-img' />
+    <Center
+      minHeight="100vh"
+      backgroundImage={`url(${login_background})`}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      paddingX={{ base: "10px", md: "20px" }}
+    >
+      <Box
+        width="100%"
+        maxWidth={{ base: "300px", md: "500px"}}
+        padding={{ base: "10px", md: "20px" }}
+        boxShadow="0 0 10px rgba(0, 0, 0, 0.1)"
+        borderRadius="8px"
+        bg="rgba(255, 255, 255, 0.8)"
+        marginTop="20px"
+      >
+        <Stack spacing={5}>
+          <Box display="flex" justifyContent="center" marginBottom="10px">
+            <Image src={website_icon} width={{ base: "200px", md: "300px" }} />
           </Box>
-          <Input className='login-input' placeholder='Name' />
-          <Input className='login-input' placeholder='Email' />
-          <Input className='login-input' placeholder='Password' />
-          <VStack spacing={3} justifyContent='center' width='100%'>  {/* Center align */}
-            <Button variant="solid" colorScheme='green' className='login-button'>Login</Button>
+          <Input placeholder="Name" fontSize="1rem" />
+          <Input placeholder="Email" fontSize="1rem" />
+          <Input placeholder="Password" fontSize="1rem" />
+          <VStack spacing={2} width="100%">
+            <Button variant="solid" colorScheme="green" width="100%" fontSize="1rem" padding="10px 20px">
+              Login
+            </Button>
             <Text>OR</Text>
-            <Button className='login-button' variant='outline'>Create an Account</Button>
+            <Button variant="outline" width="100%" fontSize="1rem" padding="10px 20px">
+              Create an Account
+            </Button>
           </VStack>
         </Stack>
       </Box>
     </Center>
-    </>
-
   );
 }
 
