ceacb5a7018f589a2286c5c091393a39b6242dc3
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-07 12:33:02 +0530

    added font and onchange to track input field of user

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -12,6 +12,10 @@ const Login = () => {
     toggleAccount(account === 'login' ? 'create' : 'login');
   };
 
+  const onInputChange = (e)=>{ 
+    console.log(e.target.name,e.target.value)
+  }
+
   return (
     <>
       <Center
@@ -32,21 +36,27 @@ const Login = () => {
         >
           <Stack spacing={5}>
             <Box display="flex" justifyContent="center" marginBottom="10px">
-              <Image src={website_icon} width={{ base: "200px", md: "300px" }} />
+              {/* <Image src={website_icon} width={{ base: "200px", md: "300px" }} /> */}
+              <Text fontFamily={"Bad Script,cursive"} fontWeight="800" fontSize="50px">MemoryOcean . .</Text>
             </Box>
 
             {/* Conditional rendering based on account state */}
+
+            
             {account === 'login' ? (
               <>
+              {/* using onchange to get value from input in console*/}
+
+              
                 <Input placeholder="Email" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} />
-                <Input placeholder="Password" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} />
+                <Input placeholder="Password" fontSize="1rem" _hover={{ borderColor: "#a27fba" }}/>
                 <Button variant="solid" colorScheme="green" width="100%" fontSize="1rem" padding="10px 20px">Login</Button>
               </>
             ) : (
               <>
-                <Input placeholder="Name" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} />
-                <Input placeholder="Email" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} />
-                <Input placeholder="Password" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} />
+                <Input placeholder='Name' fontSize="1rem" _hover={{ borderColor: "#a27fba" }} onChange={onInputChange} name='name :'/>
+                <Input placeholder="Email" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} onChange={onInputChange} name='email :'/>
+                <Input placeholder="Password" fontSize="1rem" _hover={{ borderColor: "#a27fba" }} onChange={onInputChange} name='password :'/>
                 <Button variant="solid" colorScheme="green" width="100%" fontSize="1rem" padding="10px 20px">Sign up</Button>
                 
               </>
