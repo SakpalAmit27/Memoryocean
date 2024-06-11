37de1691c501579bef219591ef8e855d9eeec8ca
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-11 08:56:27 +0530

    written configuration for error handling api

---
+++ src/constants/config.js
@@ -0,0 +1,24 @@
+// API_NOTIFICATION_MESSAGES //
+
+export const API_NOTIFICATION_MESSAGES = {
+    loading:{
+        title:"loading",
+        message:"your data is being loaded , please wait"
+    },
+    success : {
+        title:"Success",
+        message:"Data Successfully loaded"
+    },
+    responseFailure : {
+        title:"Error",
+        message:"an error occurred while fetching response from the server , please try again" 
+    },
+    requestFailure : {
+        title:"Error",
+        message:"an error occured while parsing the data "
+    },
+    networkError:{
+        title:"Error",
+        message:"Unable to connect with the server , please check your internet connectivity and try again later"
+    }
+}
